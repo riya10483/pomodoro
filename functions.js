a873fcf12b6c35cc8a3131f2d6a1ccf8e5f1af9c
Riya Rathi <riyarathi@tamu.edu>
2024-09-28 20:38:01 -0500

    add new functions

--- functions.js
+++ functions.js
@@ -1,5 +1,3 @@
-let seconds = 300
-   
 
 // setInterval(function () {
 //     seconds -= 1
@@ -8,6 +6,19 @@ let seconds = 300
 
 // }, 1000);
 
-function myFunction() {
-    console.log("hello")
+const startTiming = 20
+let time = startTiming * 60
+
+const count = document.getElementById("timer")
+
+setInterval(updateTimer, 1000)
+function updateTimer(){
+    const minutes = Math.floor(time/60)
+    let seconds = time % 60
+
+    //condition ? do this : do that
+    seconds  = seconds < 10 ?  '0' + seconds : seconds
+    count.innerHTML = '${minutes}:${seconds}'
+    time--
+
 }
