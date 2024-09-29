b222f6d2a9bd40521ee61d6b5ed797f31884a274
Riya Rathi <riyarathi@tamu.edu>
2024-09-29 09:53:54 -0500

    timer changes according to mode

--- functions.js
+++ functions.js
@@ -7,27 +7,30 @@
 // }, 1000);
 
 let startTiming = 0
-let time = startTiming * 60
 let paused = false;
 let time1,time2, time3
+let timerText = "0:00"
 
 const count = document.getElementById("timer")
 
 let interval = setInterval(updateTimer, 1000)
 
-function updateTimer(time1, time2, time3){
-    const minutes = Math.floor(time/60)
-    let seconds = time % 60
+function updateTimer(){
+
+    const minutes = Math.floor(startTiming/60)
+    let seconds = startTiming % 60 
 
     //condition ? do this : do that
     seconds  = seconds < 10 ?  '0' + seconds : seconds
-    count.innerHTML = `${minutes}:${seconds}`
-    time--
     
-    if (time <= -1)
+    timerText = `${minutes}:${seconds}`
+    
+    
+    if (startTiming <= 0)
     {
         clearInterval(interval)
-        time = -1
+        clearInterval(interval2)
+        count.innerHTML = "0:00"
     }
 
 
@@ -73,34 +76,50 @@ function pause() {
     const pauseButton = document.querySelector('.control-buttons button');
     paused = !paused;
 
-    if(paused) {
+    /*if(paused) {
         clearInterval(interval);
+        clearInterval(interval2);
         pauseButton.textContent = 'Resume';
     }
     else {
         startTimer();
         pauseButton.textContent = 'Pause';
-    }
+    }*/
 }
 
 function startTimer() {
-    interval = setInterval(updateTimer, 1000);
+    interval = setInterval(()=>{startTiming--;updateTimer()}, 1000);
+    interval2 = setInterval(()=>{count.innerHTML = timerText}, 100);
 }
 
-function chooseTime() {
-    const focusLength = document.getElementById('time1').value;
-    const longBreak = document.getElementById('time2').value;
-    const shortBreak = document.getElementById('time3').value;
-    startTiming = focusLength;
-    time = startTiming * 60;
+async function chooseTime() {
+    const focusLength = document.getElementById('time1').value *60;
+    const longBreak = document.getElementById('time2').value*60;
+    const shortBreak = document.getElementById('time3').value*60;
 
     if(focusLength!= 0 && longBreak != 0 && shortBreak != 0){
-        //updateTimer(focusLength, shortBreak, longBreak)
-        startTimer()
-    }
-    if (time <= -1) {
-        startTiming = shortBreak;
-        time = startTiming * 60;
+        function timeout(ms) {
+            return new Promise(resolve => {
+                setTimeout(resolve, ms*1000);
+            });
+        }
+        async function countdown(time4) {
+            startTiming = time4 +1
+            startTimer()
+            await timeout(time4+1)          
+        }
+                //document.getElementById('status').value = "Focus Session";
+
+        while(true){
+        await countdown(focusLength)
+        await countdown(shortBreak)
+        await countdown(focusLength)
+        await countdown(shortBreak)
+        await countdown(focusLength)
+        await countdown(shortBreak)
+        await countdown(focusLength)
+        await countdown(longBreak)
+}
     }
 }
 
